edf87b9501956ceba2b64cea468fc2123f5a7eae
jimena campos <jimena@ucol.mx>
2020-12-10 15:49:16 -0600

    factorial implemented

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
-export default class App {}
+export default class App {
+
+    factorial(numero){
+
+        let resultado = 1;
+
+        for(let i = 2; i<=numero; i++){
+
+            resultado = resultado * i;
+
+        }
+
+        return resultado;
+    }
+}
 
 let app = new App();
+
+console.log(app.factorial(1));
+console.log(app.factorial(2));
+console.log(app.factorial(3));
+console.log(app.factorial(4));
+console.log(app.factorial(5));
+console.log(app.factorial(6));
+
